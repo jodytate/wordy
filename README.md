cfeee65be7d2f1c065954785b3a00b4b02f42e39
jody <jtate@uw.edu>
2015-09-05 08:26:34 -0700

    bus factor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # wordy
+
+## word-related APIs
+
+[wordnik](https://www.wordnik.com/)
+
+[Words API](https://www.wordsapi.com/docs)
+
+[Merriam-Webster Dictionary API](http://www.dictionaryapi.com/) 1000 requests a day
+
+[Free Scrabble Dictionary](http://api.freescrabbledictionary.com/) 5000 requests per day
+
+[Pearson](http://developer.pearson.com/apis/dictionaries)
--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
   angular.module('wordyApp', [])
 
-  .controller('WordyController', function ($scope) {
+  .controller('WordyController', function ($scope, $http) {
 
     $scope.wordy = function (str) {
       var fn = function(active, rest, stuff) {
@@ -21,6 +21,28 @@
       };
       $scope.words = fn ('', str, []);
     };
+
+
+    $scope.intersection = [];
+
+    $scope.isItAWord = function (words) {
+      console.log(words);
+      $http.get('data/wordlist.txt')
+        .success(function(data){
+          // var wordlist = data.split('\n');
+          // console.log(wordlist)
+          // console.log(words)
+          // // $scope.wordlist = wordlist;
+          // var intersection = _.intersection(words, wordlist);
+          // console.log(intersection);
+          // $scope.intersection = _.intersection(words, wordlist);
+
+          $scope.intersection = _.intersection(['foo', 'bar'], ['foo', 'fozzie'])
+
+        });
+    };
+
+
 
   });
 
